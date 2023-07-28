4677162541e15232100ae71ab52c9a305b14ae53
John Turner <jturner.usa@gmail.com>
2023-07-28 03:19:37 -0400

    app-emacs/magit: configure load-path to load elisp binaries

--- app-emacs/magit/files/50magit-gentoo.el
+++ app-emacs/magit/files/50magit-gentoo.el
@@ -1,2 +1,6 @@
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'magit-status "magit" nil t)
+
+(when (and (ignore-errors (native-comp-available-p))
+           (string= comp-native-version-dir "@COMP_NATIVE_VERSION_DIR@"))
+  (add-to-list 'load-path "@NATIVELISP@"))
